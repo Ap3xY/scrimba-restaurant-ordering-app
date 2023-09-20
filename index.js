1e90b1825386eeaf0a0bafd2ff74031cf49487bc
Alex van den Berg <alexvdberg010109@gmail.com>
2023-09-20 18:08:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import { menuArray } from "./data.js";
+
+const itemContainer = document.getElementById("item-container");
+
+menuArray.forEach(function (item) {
+  itemContainer.innerHTML += `
+    <div class="item">
+        <div class="item-details">
+            <p id="emoji">${item.emoji}</p>
+        <div class="item-text">
+        <h3 id="item-name">${item.name}</h3>
+        <p id="item-ingredients">${item.ingredients.join(", ")}</p>
+        <p id="item-price">$${item.price}</p>
+        </div>
+        </div>
+        <i id="add" class="fa-solid fa-plus"></i>
+    </div>
+`;
+});
